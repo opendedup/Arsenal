1c43f541cbdd999021d289d5699a936ec22cae39
Jonathan Gramain <jonathan.gramain@scality.com>
2017-05-30 18:57:06 -0700

    + isMasterKey helper, fix unit tests with new stringified output from raft admin api

--- tests/unit/storage/metadata/bucketclient/RecordLog.js
+++ tests/unit/storage/metadata/bucketclient/RecordLog.js
@@ -5,34 +5,14 @@ const http = require('http');
 
 const Logger = require('werelogs').Logger;
 
-const { openLog } = require(
+const { RecordLogProxy } = require(
     '../../../../../lib/storage/metadata/bucketclient/RecordLog.js');
 
 /* eslint-disable max-len */
 const mockedLogResponse = `
     [
-        {
-            "db": "funbucket",
-            "entries": [
-                {
-                    "key": "coolkey",
-                    "value": "{\\"md-model-version\\":2,\\"owner-display-name\\":\\"Bart\\",\\"owner-id\\":\\"79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be\\",\\"content-length\\":174,\\"content-type\\":\\"text/plain\\",\\"last-modified\\":\\"2017-05-24T23:54:59.837Z\\",\\"content-md5\\":\\"875fbeafc5de4e9025ad844f7321f5bd\\",\\"x-amz-server-version-id\\":\\"\\",\\"x-amz-storage-class\\":\\"STANDARD\\",\\"x-amz-server-side-encryption\\":\\"\\",\\"x-amz-server-side-encryption-aws-kms-key-id\\":\\"\\",\\"x-amz-server-side-encryption-customer-algorithm\\":\\"\\",\\"location\\":[{\\"key\\":\\"088bac1ca870821d154feb15f0325dbd223e629b\\",\\"size\\":174,\\"start\\":0,\\"dataStoreName\\":\\"file\\"}],\\"acl\\":{\\"Canned\\":\\"private\\",\\"FULL_CONTROL\\":[],\\"WRITE_ACP\\":[],\\"READ\\":[],\\"READ_ACP\\":[]},\\"isDeleteMarker\\":false,\\"x-amz-meta-s3cmd-attrs\\":\\"uid:0/gname:root/uname:root/gid:0/mode:33188/mtime:1495045891/atime:1495669184/md5:875fbeafc5de4e9025ad844f7321f5bd/ctime:1495045891\\"}"
-                }
-            ],
-            "seq": 5,
-            "timestamp": "2017-05-24T23:54:59.853Z"
-        },
-        {
-            "db": "funbucket",
-            "entries": [
-                {
-                    "key": "coolkey",
-                    "type": "del"
-                }
-            ],
-            "seq": 6,
-            "timestamp": "2017-05-25T00:04:55.091Z"
-        }
+        "{\\"db\\":\\"funbucket\\",\\"entries\\":[{\\"key\\":\\"coolkey\\",\\"value\\":\\"{\\\\\\"md-model-version\\\\\\":2,\\\\\\"owner-display-name\\\\\\":\\\\\\"Bart\\\\\\",\\\\\\"owner-id\\\\\\":\\\\\\"79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be\\\\\\",\\\\\\"content-length\\\\\\":174,\\\\\\"content-type\\\\\\":\\\\\\"text/plain\\\\\\",\\\\\\"last-modified\\\\\\":\\\\\\"2017-05-24T23:54:59.837Z\\\\\\",\\\\\\"content-md5\\\\\\":\\\\\\"875fbeafc5de4e9025ad844f7321f5bd\\\\\\",\\\\\\"x-amz-server-version-id\\\\\\":\\\\\\"\\\\\\",\\\\\\"x-amz-storage-class\\\\\\":\\\\\\"STANDARD\\\\\\",\\\\\\"x-amz-server-side-encryption\\\\\\":\\\\\\"\\\\\\",\\\\\\"x-amz-server-side-encryption-aws-kms-key-id\\\\\\":\\\\\\"\\\\\\",\\\\\\"x-amz-server-side-encryption-customer-algorithm\\\\\\":\\\\\\"\\\\\\",\\\\\\"location\\\\\\":[{\\\\\\"key\\\\\\":\\\\\\"088bac1ca870821d154feb15f0325dbd223e629b\\\\\\",\\\\\\"size\\\\\\":174,\\\\\\"start\\\\\\":0,\\\\\\"dataStoreName\\\\\\":\\\\\\"file\\\\\\"}],\\\\\\"acl\\\\\\":{\\\\\\"Canned\\\\\\":\\\\\\"private\\\\\\",\\\\\\"FULL_CONTROL\\\\\\":[],\\\\\\"WRITE_ACP\\\\\\":[],\\\\\\"READ\\\\\\":[],\\\\\\"READ_ACP\\\\\\":[]},\\\\\\"isDeleteMarker\\\\\\":false,\\\\\\"x-amz-meta-s3cmd-attrs\\\\\\":\\\\\\"uid:0/gname:root/uname:root/gid:0/mode:33188/mtime:1495045891/atime:1495669184/md5:875fbeafc5de4e9025ad844f7321f5bd/ctime:1495045891\\\\\\"}\\"}]}",
+        "{\\"db\\":\\"funbucket\\",\\"entries\\":[{\\"key\\":\\"coolkey\\",\\"type\\":\\"del\\"}]}"
     ]
 `;
 /* eslint-enable max-len */
@@ -51,11 +31,10 @@ describe('raft record log client', () => {
         });
         server.listen(6677);
 
-        openLog({ url: 'http://localhost:6677/mockLog', logger: cliLogger },
-                (err, logProxy) => {
-                    logClient = logProxy;
-                    done();
-                });
+        logClient = new RecordLogProxy({
+            url: 'http://localhost:6677/mockLog', logger: cliLogger
+        });
+        done();
     }
 
     before(done => {
@@ -76,13 +55,11 @@ describe('raft record log client', () => {
                 assert.strictEqual(data.entries.length, 1);
                 const entry = data.entries[0];
                 if (nbRecords === 0) {
-                    assert.strictEqual(data.seq, 5);
                     assert.strictEqual(entry.type, 'put');
                     assert.strictEqual(entry.key, 'coolkey');
                     assert(entry.value.length > 0);
                 } else {
                     assert.strictEqual(nbRecords, 1);
-                    assert.strictEqual(data.seq, 6);
                     assert.strictEqual(entry.type, 'del');
                     assert.strictEqual(entry.key, 'coolkey');
                     assert.strictEqual(entry.value, undefined);
