2366c2ad858d034f0e31b6ca75e9a6094e268e90
Jonathan Gramain <jonathan.gramain@scality.com>
2017-09-06 16:45:34 -0700

    ft: ARN object model class
    Will be used by backbeat for roles, and potentially can be used for other
    components that manipulate ARNs.

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ module.exports = {
     models: {
         BucketInfo: require('./lib/models/BucketInfo'),
         ObjectMD: require('./lib/models/ObjectMD'),
+        ARN: require('./lib/models/ARN'),
         WebsiteConfiguration: require('./lib/models/WebsiteConfiguration'),
         ReplicationConfiguration:
           require('./lib/models/ReplicationConfiguration'),
